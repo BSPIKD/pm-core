4df4a8d21875cd9ef180bbbf496e87359f26b5a7
Dominik Mandinec <mandinec53@gmail.com>
2022-05-08 18:21:12 +0200

    Přidání debug configů

---
+++ src/_migrations/server/20220508123208_InsertDebugConfig.sql
@@ -0,0 +1,5 @@
+insert into config (`key`, description, value, is_important)
+values ('print-debug', 'Nastavení výpisu local printů', 1, 1);
+
+insert into config (`key`, description, value, is_important)
+values ('print-highlight', 'Zapnout highlight u local printů', 1, 1);
